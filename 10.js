c2d85d37e12a6833d4f457e936ca09bbfb5df62d
developerShafiqual <classicalshafi2@gmail.com>
2023-01-22 12:02:52 -0800

    first commit

---
+++ 10.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+  
+
+    $(".three").hover(function(){
+        alert("Welcome to my world");
+    });
+
+});
+
---
+++ HuBLU/Project 8/8.js
@@ -0,0 +1,7 @@
+
+  
+$(document).ready(function(){
+$(".box3").fadeOut('slow',) .fadeIn(3000) .fadeOut('slow',).show(3000)
+$(".box3").fadeOut('slow',) .fadeIn(3000) .fadeOut('infinite',).show(3000).sideUp(3000).sideDown(2000)
+});
+
